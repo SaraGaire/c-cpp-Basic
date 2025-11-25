4a0ca0443c5ad4c6fa54b070b5bc577af33243fb
Sara.G <151964803+SaraGaire@users.noreply.github.com>
2025-11-25 12:59:37 +0100

    Create game.h for game functions
    Added header file for game functions and modularity.

---
+++ game.h
@@ -0,0 +1,10 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "tree.h"
+
+int get_yes_no(const char *msg);
+void play_game(tree_node **root);
+
+#endif
+//a header files for thr c program , seperation for the modularilty of the code.
